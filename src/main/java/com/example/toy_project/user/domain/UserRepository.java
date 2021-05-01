ce9f8d11c90ea889507895c50a7dcf2ce3ada35a
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-05-01 23:54:14 +0900

    fix: 찾는 User가 존재한다면 찾는 User반환, 그렇지 않다면 생성

--- src/main/java/com/example/toy_project/user/domain/UserRepository.java
+++ src/main/java/com/example/toy_project/user/domain/UserRepository.java
@@ -1,6 +1,11 @@
 package com.example.toy_project.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("select U from User U where U.name = ?1")
+    Optional<User> findByName(String name);
 }
--- src/main/java/com/example/toy_project/user/service/UserService.java
+++ src/main/java/com/example/toy_project/user/service/UserService.java
@@ -7,15 +7,24 @@ import com.example.toy_project.user.domain.UserRepository;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
+    @Transactional
     public UserPostResponse save(UserPostRequest request){
-        User user = userRepository.save(new User(request));
-        return new UserPostResponse(user);
+        Optional<User> findUser = userRepository.findByName(request.getName());
+        System.out.println(findUser);
+        if (findUser == null) {
+            // 찾는 User가 없으면 새로 만들어 줌
+            return new UserPostResponse(userRepository.save(new User(request)));
+        }
+        return new UserPostResponse(findUser.get());
     }
 
 }
