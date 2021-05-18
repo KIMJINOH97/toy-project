c559f15080746c8a04339745a3f332ab37216426
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-05-18 16:30:09 +0900

    fix: dummy data 넣음

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
--- insert into order_list (name, price, count)
--- values ('감자', 1000, 10000),
--- ('딸기', 2000, 20000),
--- ('수박', 3000, 10000);
---
--- insert into user (name)
--- values ('jino'),
---        ('youngjin');
+insert into order_list (name, price, count)
+values ('감자', 1000, 10000),
+('딸기', 2000, 20000),
+('수박', 3000, 10000);
+
+insert into user (name)
+values ('jino'),
+       ('youngjin');
 SELECT * from User;
