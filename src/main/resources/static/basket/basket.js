19ddcffbd4b0463f85a2bd668bcab781bc3db42e
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-05-26 09:40:51 +0900

    chore: 로그인된 사용자만 보여주기

--- src/main/resources/static/basket/basket.js
+++ src/main/resources/static/basket/basket.js
@@ -2,9 +2,14 @@ const $ = (cls) => document.querySelector(cls);
 
 let basketList = $('.basket-list');
 let orderButton = document.querySelectorAll('.order-title')[1];
+let userId = localStorage.getItem('user-id');
+
+if (userId === null){
+    alert("로그인 후 이용해주세요");
+    window.location.href= '/';
+}
 
 async function getBasket(){
-    console.log("hi");
     const userId = localStorage.getItem("user-id");
     const response = await fetch(`/api/user/${userId}/basket`);
     const { data } = await response.json();
@@ -30,7 +35,7 @@ async function deleteBasket(orderId){
 
         let response = await fetch(`/api/user/${userId}/basket/${orderId}`, {method: 'DELETE'});
         let {status_code, message} = await response.json();
-        console.log(status_code, message);
+
         if (status_code == 200) {
             alert(message);
             window.location.href = '/basket';
@@ -42,19 +47,23 @@ async function deleteBasket(orderId){
     }
 }
 
+function goItem(orderId){
+    localStorage.setItem("item-number", orderId);
+    window.location.href = './order-item';
+}
+
 const BasketTemplate = (order) => `
     <li class="order-item">
-        <div class="item">${order.order_id}</div>
-        <div class="item">
-            <img class="order-picture" src="dummy.png" />
+        <div class="order-content" type="button" onClick="goItem(${order.order_id})">
+            <div class="item">${order.order_id}</div>
+            <div class="item">
+                <img class="order-picture" src="dummy.png"/>
+            </div>
+            <div class="item">${order.order_name}</div>
+            <div class="item">${order.price}</div>
         </div>
-        <div class="item">${order.order_name}</div>
-        <div class="item">${order.price}</div>
-        <div class="item">
-            <input type="button" class="pick-button" value="삭제" onclick="deleteBasket(${order.order_id})"/>
-        </div>
+        <input type="button" class="pick-button" value="삭제" onclick="deleteBasket(${order.order_id})"/>
     </li>
 `;
-
 
 getBasket();
